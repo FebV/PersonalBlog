ea96fdc9479f6cec2eec8cf6a70709708419c167
FebV <cn.zhaoyifan@gmail.com>
2017-03-13 23:10:37 +0800

    fix responsive error

--- controllers/ResponsiveController.js
+++ controllers/ResponsiveController.js
@@ -1,11 +1,12 @@
 export default class ResponsiveController {
     constructor() {
-        if(matchMedia('(orientation:landscape)').matches === true)
+        if(matchMedia('(orientation:landscape)').matches == true)
             this.direction = 'landscape';
         else
             this.direction = 'portrait';
+
         window.addEventListener('resize', e => {
-            if(!matchMedia(`(orientation:${this.direction}`).matches) {
+            if(!matchMedia(`(orientation:${this.direction})`).matches) {
                 this.direction = this.direction === 'landscape' ? 'portrait' : 'landscape';
                 dispatchEvent(new Event('rotate'));
                 location.reload();
